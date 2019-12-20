82a26de8c445186623834cb33e67eb924fd26d6d
victoriagjh <akrso06197@khu.ac.kr>
2019-12-20 02:41:18 -0800

    connect react-native with koaServer
    - get the openAPI response from the koaServer

--- koaServer/src/api/info/info.controller.js
+++ koaServer/src/api/info/info.controller.js
@@ -4,17 +4,16 @@ const NodeRSA = require('node-rsa');
 
 const key = require('./secrets.json').apiKey;
 
-exports.list = (ctx) => {
-  console.log(key);
-  request('http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?serviceKey=' +key+'&numOfRows=10&pageNo=1&stationName=%EC%A2%85%EB%A1%9C%EA%B5%AC&dataTerm=DAILY&ver=1.3&_returnType=json',function(error, response, body){
+exports.list = async (ctx) => {
+  let response = await request('http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?serviceKey=' + key +'&numOfRows=10&pageNo=1&stationName=%EC%A2%85%EB%A1%9C%EA%B5%AC&dataTerm=DAILY&ver=1.3&_returnType=json',function(error, response, body){
     if(!error&&response.statusCode==200) {
-      var $=cheerio.load(body)
-      console.log(body);
-      ctx.body = body;
+      var $=cheerio.load(body);
+      console.log(response.body);
     } else {
       console.log(error);
     }
   });
+  ctx.body = response;
 };
 
 exports.create = (ctx) => {
