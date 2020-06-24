6cdb48130b3036fd9d627d501bb05dbd00ec7db6
권주희 <akrso06197@khu.ac.kr>
2020-06-24 16:38:27 +0900

    Update API.js

--- what_is_dust/src/API.js
+++ what_is_dust/src/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL : "http://localhost:4000",
-  responseType : "json"
+  baseURL: "http://oss.khunet.net:4000",
+  responseType: "json",
 });
